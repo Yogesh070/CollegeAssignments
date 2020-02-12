95e48e9565e51baedaa6455cbb691e46e49c4427
Yogesh thapa <ythapa430@gmail.com>
2020-02-12 17:26:39 +0545

    Added my programs for lab1

---
+++ Lab1/Program/MultiDimensional.java
@@ -0,0 +1,11 @@
+class MultiDimensional{  
+public static void main(String args[]){  
+//declaring and initializing 2D array  
+int arr[][]={{1,2,3},{2,4,5},{4,4,5}};
+for(int i=0;i<3;i++){  
+ for(int j=0;j<3;j++){  
+   System.out.print(arr[i][j]+" ");  
+ }  
+ System.out.println();  
+}  
+}}
---
+++ Lab1/Program/SingleDimensional.java
@@ -0,0 +1,8 @@
+//demonstration of single dimenstional array
+
+class SingleDimensional{  
+public static void main(String args[]){  
+int a[]={33,3,4,5};
+for(int i=0;i<a.length;i++)//length is the property of array  
+System.out.println(a[i]);  
+}} 
