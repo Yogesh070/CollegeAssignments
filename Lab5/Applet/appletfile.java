79eeb77dd13bf97b8f58b703c9d8d2999144ad00
Yogesh thapa <ythapa430@gmail.com>
2020-03-11 21:55:26 +0545

    added my applet program

---
+++ Lab5/Applet/appletfile.java
@@ -0,0 +1,9 @@
+import java.applet.Applet;  
+import java.awt.Graphics;  
+public class appletfile extends Applet{  
+  
+public void paint(Graphics g){  
+g.drawString("welcome",150,150);  
+}  
+  
+}
